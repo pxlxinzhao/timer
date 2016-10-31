cba00864e1f6f2b5b4cd5afc2ed128fd69221dbf
patrickpu <pxlxinzhao@gmail.com>
2016-10-31 17:00:59 -0400

    。。

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,7 @@ export class HomePage {
   interval: any = null;
   isCounting: boolean = false;
 
-  timeCounter: string;
+  timeCounter: string = "00:00:00:000";
 
   hours: string = "00"
   minutes: string = "00";
@@ -20,20 +20,14 @@ export class HomePage {
   miniSeconds:string = "000";
 
   constructor(public navCtrl: NavController) {
-    this.updateTimeCounter();
-  }
-
-  updateTimeCounter() {
-    this.timeCounter = this.hours + ":" + this.minutes + ":" + this.seconds + ":" + this.miniSeconds;
   }
 
   toggleTimer(){
     let self = this;
 
-    if (!this.isCounting){
-      this.isCounting = true;
-      this.timeBase = new Date().getTime();
-      console.log('timeBase', this.timeBase);
+    if (!self.isCounting){
+      self.isCounting = true;
+      self.timeBase = new Date().getTime();
 
       this.interval = setInterval(function(){
 
@@ -45,20 +39,44 @@ export class HomePage {
         self.seconds = Math.floor(diff/1000) + "";
         self.miniSeconds = diff%1000 + "";
 
-        this.updateTimeCounter();
-      }, 100)
+        updateTimeCounter();
+      }, 1000)
     }else{
       clearInterval(this.interval);
 
-      this.isCounting = false;
-      this.timeBase = null;
+      self.isCounting = false;
+      self.timeBase = null;
 
       self.hours = "00"
       self.minutes = "00";
       self.seconds = "00";
       self.miniSeconds = "000";
 
-      this.updateTimeCounter()
+      updateTimeCounter();
+    }
+
+    function updateTimeCounter() {
+      //console.log('hours', self.hours);
+      //console.log('minutes', self.minutes);
+      //console.log('seconds', self.seconds);
+      prependZeros();
+      self.timeCounter = self.hours + ":" + self.minutes + ":" + self.seconds + ":" + self.miniSeconds;
+    }
+
+    function prependZeros(){
+      self.hours = prependZero(self.hours);
+      self.minutes = prependZero(self.minutes);
+      self.seconds = prependZero(self.seconds);
+      self.miniSeconds = prependZero(self.miniSeconds);
+    }
+
+    function prependZero(it){
+      //console.log('before', it);
+      if (it.length === 1){
+        it = "0" + self.hours;
+      }
+      //console.log('after', it);
+      return it;
     }
   }
 }
